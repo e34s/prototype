b35fb0d51636147c519c65b375f93d2d63a6130e
palotas <michael.palotas@element34.com>
2018-03-01 11:44:59 +0100

    data provider now also as map

--- src/test/java/axa/utils/ExcelAdapter.java
+++ src/test/java/axa/utils/ExcelAdapter.java
@@ -8,12 +8,41 @@ package axa.utils;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ExcelAdapter {
+
+
+	@DataProvider(name = "datamap", parallel = true)
+	public Object[][] dataSupplier() throws IOException {
+		File file = new File("C:\\Users\\mpalotas\\tmp\\axadata.xlsx");
+		FileInputStream fis = new FileInputStream(file);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0);
+		wb.close();
+		int lastRowNum = sheet.getLastRowNum() ;
+		int lastCellNum = sheet.getRow(0).getLastCellNum();
+		Object[][] obj = new Object[lastRowNum][1];
+		for (int i = 0; i < lastRowNum; i++) {
+			Map<Object, Object> datamap = new HashMap<>();
+			for (int j = 0; j < lastCellNum; j++) {
+				datamap.put(sheet.getRow(0).getCell(j).toString(), sheet.getRow(i+1).getCell(j).toString());
+			}
+			obj[i][0] = datamap;
+		}
+		return  obj;
+	}
+
+
+
 
 	@DataProvider(name = "data", parallel = true)
 	public Object[][] getExcelData() throws InvalidFormatException, IOException{
